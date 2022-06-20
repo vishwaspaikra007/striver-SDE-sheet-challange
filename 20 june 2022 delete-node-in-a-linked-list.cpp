eb318c4c2cfa86e843e15e2a650016f685427b9c
Vishwas Paikra <vishwaspaikra007@gmail.com>
2022-06-20 19:43:48 +0530

    codestudio
    20 june 2022 delete-node-in-a-linked-list

---
+++ 20 june 2022 delete-node-in-a-linked-list.cpp
@@ -0,0 +1,27 @@
+// striver SDE sheet challange delete-node-in-a-linked-list
+// author Vishwas Paikra
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+    node->data = node->next->data;
+    node->next = node->next->next;
+}

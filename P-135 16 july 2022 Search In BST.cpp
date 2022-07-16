8af7b22e51017e07d27e0923110364d2a8b1c3c3
Vishwas Paikra <vishwaspaikra007@gmail.com>
2022-07-16 23:40:43 +0530

    codestudio P-135 Search In BST
    codestudio P-135 16 july 2022 Search In BST

---
+++ P-135 16 july 2022 Search In BST.cpp
@@ -0,0 +1,27 @@
+// striver SDE sheet challange Search In BST
+// author Vishwas Paikra
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    while(root) {
+        if(root->data == x) return true;
+        else if(root->data > x) root = root->left;
+        else root = root->right;
+    }
+    return false;
+}
